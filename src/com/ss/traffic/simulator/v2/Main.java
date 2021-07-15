512d1c70c01bc5c664edb04d7e99eeba39f4e7ea
Solo <shihundusolomon@gmail.com>
2021-07-15 23:56:29 +0300

    removed extrantenuos window class - not necessary

--- src/com/ss/traffic/simulator/v2/Main.java
+++ src/com/ss/traffic/simulator/v2/Main.java
@@ -2,10 +2,10 @@ package com.ss.traffic.simulator.v2;
 
 import javax.swing.*;
 
-public class Main {
-
+public class Main extends JFrame
+{
     public static void main(String args[])
     {
-        SwingUtilities.invokeLater(() -> new Window());
+        
     }
 }
